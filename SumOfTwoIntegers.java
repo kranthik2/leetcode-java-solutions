ee0cdd098ee409a48f0d3248717ac4bf30120666
Kranthi kulli <kullik2@gmail.com>
2022-06-28 22:03:52 -0500

    Update SumOfTwoIntegers.java

--- SumOfTwoIntegers.java
+++ SumOfTwoIntegers.java
@@ -2,6 +2,13 @@
 * Given two integers a and b, return the sum of the two integers without using the operators + and -.
 */
 public class SumOfTwoIntegers{
+    
+    /***
+    * To Refresh bitwise operations :  https://en.wikipedia.org/wiki/Bitwise_operation
+    *The sum of two numbers can be obtained by performing the XOR (^) operation on these numbers 
+    * and adding a carry bit that is obtained by performing the AND (&) operation.
+    */
+    
     public int getSum(int a, int b){
       while(b != 0){
         //bitwise AND and leftshift bits by 1
