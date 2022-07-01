43e1cac4b327d1ae537816defdd504bbe4f29c56
Kranthi kulli <kullik2@gmail.com>
2022-06-30 21:42:43 -0500

    Update LongestSubString.java

--- strings/LongestSubString.java
+++ strings/LongestSubString.java
@@ -6,9 +6,10 @@ import java.util.HashSet;
 /***
 * This solution similar to sliding window solution.
 * 1) start two pointers from begining of string
-* 2) check in set, if not dupicate, store non duplicate value in set 
-* 3) move pointer to next and compare size of set and max so far, set which ever is max
+* 2) check current character in set, if not dupicate store non duplicate value in set 
+* 3) move right pointer to next and compare size of set and max so far, set which ever is max
 * 4) if set contains key, you remove duplicate and move left pointer to next character.
+* 5) repeat steps 2 to 4 until right pointer reaches end of string
 */
 public class LongestSubString{
   public int solution(String s){
