8b68b1f92791c8209f91c040e18545be0a379c6b
Kranthi kulli <kullik2@gmail.com>
2022-07-01 18:42:01 -0500

    Create LowestCommonAncestor.java

---
+++ trees/LowestCommonAncestor.java
@@ -0,0 +1,19 @@
+public class LowestCommonAncestor {
+    public TreeNode solution(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = solution(root.left, p, q);
+        TreeNode right = solution(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left == null && right == null) {
+            return null;
+        } else {
+            return left == null ? right : left;
+        }
+    }
+}
